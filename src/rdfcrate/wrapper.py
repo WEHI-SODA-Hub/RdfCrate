3cc5a1ade746e1e84aeb237d23cb867193c2d84d
Michael Milton <michael.r.milton@gmail.com>
2025-01-14 16:51:23 +1100

    Update docstrings and child signatures to use tuples

--- src/rdfcrate/wrapper.py
+++ src/rdfcrate/wrapper.py
@@ -27,7 +27,7 @@ class RoCrate:
     #: Version of the RO-Crate specification to use
     version: SpecVersion = field(kw_only=True, default=ROCrate1_1)
 
-    def add_entity(self, id: IdentifiedNode, type: URIRef, attrs: Attributes = []):
+    def add_entity(self, id: IdentifiedNode, type: URIRef, attrs: Attributes = []) -> IdentifiedNode:
         """
         Adds any type of entity to the crate
 
@@ -35,6 +35,9 @@ class RoCrate:
             id: ID of the entity being added
             type: Type of the entity being added
             attrs: Attributes of the entity being added
+
+        Returns:
+            The ID of the new entity
 
         Example:
             ```python
@@ -46,10 +49,13 @@ class RoCrate:
         """
         self.graph.add((id, RDF.type, type))
         self.add_metadata(id, attrs)
+        return id
     
-    def register_file(self, path: str, attrs: Attributes = [], guess_mime: bool = True, **kwargs: Any):
+    def register_file(self, path: str, attrs: Attributes = [], guess_mime: bool = True, **kwargs: Any) -> IdentifiedNode:
         """
         Adds file metadata to the crate
+
+        Returns: The ID of the new file entity
 
         Params:
             path: Path or URL to the file being added
@@ -64,20 +70,29 @@ class RoCrate:
             guess_type, _guess_encoding = mimetypes.guess_type(path)
             if guess_type is not None:
                 self.add_metadata(file_id, [(uris.encodingFormat, Literal(guess_type))])
+        return file_id
 
-    def register_dir(self, path: str, attrs: Attributes = {}, **kwargs: Any):
+    def register_dir(self, path: str, attrs: Attributes = {}, **kwargs: Any) -> IdentifiedNode:
         """
         Adds metadata for a directory
+
+        Returns:
+            The ID of the new directory entity
 
         Params:
             path: Path to the directory, which must be within the crate root
             attrs: Attributes used to describe the `Dataset` entity
         """
-        self.add_entity(URIRef(path), uris.Dataset, attrs)
+        dir_id = URIRef(path)
+        self.add_entity(dir_id, uris.Dataset, attrs)
+        return dir_id
 
-    def add_metadata(self, entity: IdentifiedNode, attrs: Attributes):
+    def add_metadata(self, entity: IdentifiedNode, attrs: Attributes) -> IdentifiedNode:
         """
         Add metadata for an existing entity.
+
+        Returns:
+            The ID of the updated entity
 
         Params:
             entity: ID of the entity being described
@@ -85,6 +100,7 @@ class RoCrate:
         """
         for pred, obj in attrs:
             self.graph.add((entity, pred, obj))
+        return entity
 
     def compile(self) -> str:
         """
@@ -143,7 +159,7 @@ class AttachedCrate(RoCrate):
 
         return path.relative_to(self.root).as_posix()
 
-    def register_file(self, path: Path | str, attrs: Attributes = {}, guess_mime: bool = True, add_size: bool = False, **kwargs: Any):
+    def register_file(self, path: Path | str, attrs: Attributes = [], guess_mime: bool = True, add_size: bool = False, **kwargs: Any):
         """
         See [`RoCrate.register_file`][rdfcrate.wrapper.RoCrate.register_file].
 
@@ -152,13 +168,14 @@ class AttachedCrate(RoCrate):
         """
         path = Path(path)
         file_id = self._resolve_path(path)
-        super().register_file(file_id, attrs, guess_mime)
+        ret = super().register_file(file_id, attrs, guess_mime)
         if add_size:
             if has_predicate(attrs, uris.contentSize):
                 raise ValueError("Cannot add size when contentSize is provided")
             self.add_metadata(URIRef(file_id), [(uris.contentSize, Literal(stat(path).st_size))])
+        return ret
 
-    def register_dir(self, path: Path | str, attrs: Attributes = {}, recursive: bool = False, **kwargs: Any):
+    def register_dir(self, path: Path | str, attrs: Attributes = [], recursive: bool = False, **kwargs: Any):
         """
         See [`RoCrate.register_dir`][rdfcrate.wrapper.RoCrate.register_dir].
 
@@ -166,7 +183,7 @@ class AttachedCrate(RoCrate):
             recursive: If true, register all files and subdirectories in the directory. The automatically created children will only have default metadata.
                 You can use `add_metadata` to document them more comprehensively.
         """
-        super().register_dir(self._resolve_path(path), attrs)
+        id = super().register_dir(self._resolve_path(path), attrs)
 
         if recursive:
             for child in Path(path).iterdir():
@@ -175,6 +192,8 @@ class AttachedCrate(RoCrate):
                     self.register_dir(child, recursive=True)
                 else:
                     self.register_file(child)
+
+        return id
 
 @dataclass
 class DetatchedCrate(RoCrate):

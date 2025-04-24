3d76cbb98174bcc2604a199ef5fce62e03865ef2
Michael Milton <michael.r.milton@gmail.com>
2025-04-24 11:48:38 +1000

    Linter fixes

--- src/rdfcrate/__init__.py
+++ src/rdfcrate/__init__.py
@@ -1,4 +1,4 @@
 from rdfcrate.wrapper import AttachedCrate, DetatchedCrate
-from rdfcrate.vocabs import *
+from rdfcrate.vocabs import bioschemas, bioschemas_drafts, dc, geo, owl, pav, pcdm, prof, prov, rdf, rdfs, roc, rocrate, schemaorg, sdo
 
 __all__ = ["DetatchedCrate", "AttachedCrate"]

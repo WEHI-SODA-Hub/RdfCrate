b24c7128ad4d22bb82bac18fc5a2ec59ea8fba09
Michael Milton <michael.r.milton@gmail.com>
2025-04-24 14:07:27 +1000

    Linter

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -1,4 +1,4 @@
-from rdflib import RDFS, Graph, RDF, URIRef, PROV
+from rdflib import RDFS, Graph, RDF, PROV
 
 from rdfcrate.codegen import find_classes, find_datatypes, find_enum_values, find_properties, SDO
 

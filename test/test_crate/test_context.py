f5243ec54e934b86b818f94cf0cd12bad7e88796
Michael Milton <michael.r.milton@gmail.com>
2025-08-08 15:36:08 +1000

    Move some tests into a ContextGraph specific test suite

---
+++ test/test_crate/test_context.py
@@ -0,0 +1,155 @@
+"""
+Tests for ContextGraph, ie those that aren't RO-Crate specific.
+"""
+
+from rdfcrate.context_graph import ContextGraph
+from rdfcrate import RdfProperty, owl, RdfClass
+from rdflib import RDF, Literal, BNode, URIRef
+from rdfcrate.rdfterm import RdfTerm
+from rdfcrate.vocabs import dc, sdo, rdf, bioschemas_drafts, rdfs
+import json
+import pytest
+
+
+def test_bioschemas():
+    graph = ContextGraph()
+    graph.add_entity(
+        bioschemas_drafts.LabProtocol("#some_protocol"),
+        sdo.name(sdo.Text("Some Protocol")),
+    )
+    crate_json = json.loads(graph.compile())
+    for entity in crate_json["@graph"]:
+        assert "http" not in entity["@type"], "All terms should be shortened"
+
+    assert crate_json["@context"] == [
+        "https://w3id.org/ro/crate/1.1/context",
+        {"LabProtocol": str(bioschemas_drafts.LabProtocol.term.uri)},
+    ], "Only the terms that are used in the graph should be in the context"
+
+
+def test_bnode():
+    graph = ContextGraph()
+    graph.add_entity(bioschemas_drafts.LabProtocol(BNode()))
+    assert any(isinstance(id, BNode) for id in graph.graph.subjects())
+
+
+def test_multi_type():
+    class CustomType(rdfs.Class):
+        term = RdfTerm("https://example.org/SomeOtherType")
+
+    graph = ContextGraph()
+    graph.add_entity(
+        bioschemas_drafts.LabProtocol("#multi_type_entity"),
+        CustomType.to_type_property(),
+    )
+
+    # We never want to redefine rdf:type
+    assert str(rdf.type.term.uri) not in graph._custom_terms
+    # But we should have the custom type in the context
+    assert "https://example.org/SomeOtherType" in graph.full_context.to_dict().values()
+
+
+def test_redefine_term():
+    graph = ContextGraph()
+    with pytest.raises(ValueError):
+        graph.register_term(
+            RdfTerm("https://example.org/Thing", "Thing"),
+        )
+
+
+def test_add_metadata_term():
+    """
+    Test that terms used in `add_metadata` are registered in the context.
+    """
+    graph = ContextGraph()
+    graph.add_entity(
+        sdo.Thing("#thing"),
+        dc.valid(rdfs.Literal(Literal(True))),
+    )
+    # If valid can expand, it means it was correctly registered
+    assert graph.full_context.expand("valid") == "http://purl.org/dc/terms/valid"
+
+
+def test_reverse_prop():
+    """
+    Test that we can add a reverse property to an entity.
+    """
+    graph = ContextGraph()
+    thing = graph.add_entity(
+        sdo.Thing("#thing"),
+    )
+    other_thing = graph.add_entity(
+        sdo.Thing("#other-thing"), sdo.hasPart.reverse(thing)
+    )
+    assert (
+        thing.id,
+        sdo.hasPart.term.uri,
+        other_thing.id,
+    ) in graph.graph
+
+
+def test_adhoc_term():
+    """
+    Test that we can define terms on the fly and use them in properties.
+    """
+    graph = ContextGraph()
+    thing = graph.add_entity(
+        sdo.Thing("#thing"),
+        RdfProperty.adhoc(
+            term=RdfTerm("https://example.org/myProp"), object=sdo.Text("value")
+        ),
+    )
+    assert (
+        thing.id,
+        URIRef("https://example.org/myProp"),
+        Literal("value"),
+    ) in graph.graph
+
+
+def test_cls_with_term_label():
+    """
+    Test that we can use two types with the same term label, by renaming one of them.
+    """
+    graph = ContextGraph()
+    thing = graph.add_entity(
+        sdo.Class("#thing"), owl.Class.with_term_label("owlClass").to_type_property()
+    )
+    assert (thing.id, RDF.type, sdo.Class.term.uri) in graph.graph
+    assert (thing.id, RDF.type, owl.Class.term.uri) in graph.graph
+
+
+def test_prop_with_term_label():
+    """
+    Test that we can use two properties with the same term label, by renaming one of them.
+    """
+    graph = ContextGraph()
+
+    class exHasPart(RdfProperty):
+        term = RdfTerm("https://example.org/hasPart", "hasPart")
+
+    part = sdo.CreativeWork("https://example.org/part")
+
+    with pytest.raises(ValueError):
+        graph.add_entity(
+            sdo.Thing("#thing"),
+            sdo.hasPart(part),
+            exHasPart(part),
+        )
+
+    graph.add_entity(
+        sdo.Thing("#thing"),
+        sdo.hasPart(part),
+        exHasPart.with_term_label("exHasPart")(part),
+    )
+
+
+def test_adhoc_class():
+    """
+    Test that we can use two types with the same term label, by renaming one of them.
+    """
+    graph = ContextGraph()
+    ExampleClass = RdfClass.adhoc(RdfTerm("https://example.org/thing"))
+    graph.add_entity(
+        ExampleClass("#thing"),
+    )
+    assert (URIRef("#thing"), RDF.type, ExampleClass.term.uri) in graph.graph

ec8df603cfb3b00c8d120e23f7f4326bee0be1fa
Michael Milton <michael.r.milton@gmail.com>
2026-09-16 14:15:14 +1000

    Ruff format

--- src/rdfcrate/codegen/__init__.py
+++ src/rdfcrate/codegen/__init__.py
@@ -314,7 +314,7 @@ class CodegenState:
                         ast.Subscript(
                             value=ast.Name("RdfProperty", ctx=ast.Load()),
                             slice=self.property_range(prop),
-                            ctx=ast.Load()
+                            ctx=ast.Load(),
                         )
                     ],
                     keywords=[],
@@ -523,7 +523,7 @@ class CodegenState:
                             ast.Subscript(
                                 value=ast.Name("RdfProperty", ctx=ast.Load()),
                                 slice=self.property_range(URIRef(uri)),
-                                ctx=ast.Load()
+                                ctx=ast.Load(),
                             )
                         ],
                         keywords=[],
